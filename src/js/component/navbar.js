0cebe37d46c00cf09c8702706a98a94fb9c86666
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2024-09-09 09:25:12 +0000

    style navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,11 @@ export const Navbar = () => {
 
 
 	return (
-		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+		<nav className="navbar navbar-expand-lg bg-dark">
   <div className="container-fluid">
     {/* <a className="navbar-brand" href="#"><img src = "https://i.imgur.com/YhauhRo.png" alt="Star Wars logo" width="100px" style={{ marginLeft: '20px' }}/></a> */}
     <Link className="navbar-brand" to="/">
-          <img src="https://i.imgur.com/YhauhRo.png" alt="Logo Star Wars" width="100px" style={{ marginLeft: '20px' }} />
+          <img src="https://i.imgur.com/YhauhRo.png" alt="Logo Star Wars" width="70px" style={{ marginLeft: '20px' }} />
         </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -18,18 +18,18 @@ export const Navbar = () => {
       <ul className="navbar-nav">
         <li className="nav-item">
           {/* <a className="nav-link active" aria-current="page" href="#">Characters</a> */}
-		  <Link className=" btn btn-outline-dark me-2" aria-current="page" to="/characters">Characters</Link>
+		  <Link className=" btn btn-secondary me-2" aria-current="page" to="/characters">Characters</Link>
         </li>
         <li className="nav-item">
 		{/* <a className="nav-link active" aria-current="page" href="#">Planets</a> */}
-		<Link className=" btn btn-outline-dark me-2" aria-current="page" to="/planets">Planets</Link>
+		<Link className=" btn btn-secondary  me-2" aria-current="page" to="/planets">Planets</Link>
         </li>
         <li className="nav-item">
 		{/* <a className="nav-link active" aria-current="page" href="#">Starships</a> */}
-		<Link className=" btn btn-outline-dark me-2" aria-current="page" to="/starships">Starships</Link>
+		<Link className=" btn btn-secondary  me-2" aria-current="page" to="/starships">Starships</Link>
         </li>
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a className="nav-link btn-warning me-2 dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Favorites
           </a>
           <ul className="dropdown-menu">
